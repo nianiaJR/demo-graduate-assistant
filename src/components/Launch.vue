b16167bd8f8a7d3ea6b1e0d88becc885760995ff
梁家瑞 <ljracm@gmail.com>
2017-03-19 18:37:40 +0800

    ADD: LACUNCH

--- src/components/Launch.vue
+++ src/components/Launch.vue
@@ -2,6 +2,59 @@
     .launch
         rd-swipe(:swipe="swipe")
             .rd-swipe-item(:style="{'background-image': `url(${img.url})`}" v-for="(img, index) in imgs")
+        .intro
+            ul
+                li
+                    img(src="../assets/profileicon.png")
+                    h3 SHOWCASE
+                    p TextTextTex
+                li
+                    img(src="../assets/exploreicon.png")
+                    h3 SEARCH
+                    p TextTextTex
+                li
+                    img(src="../assets/connecticon.png")
+                    h3 CONNECT
+                    p TextTextTex
+            .login
+                .wrap
+                    h3 Login
+                    input(type="text" placeholder="Username")
+                    input(type="text" placeholder="Password")
+                    p or Sign Up for FREE
+        ul.contact
+            li
+                h3 ABOUT US
+                img.logo(src="../assets/logo.jpg" width="100" height="100")
+                p
+                    span Canvas Education is the first
+                    br
+                    span holistic education platform that
+                    br
+                    span connects Students in Asia to
+                    br
+                    span Universities overseas.
+            li.contact
+                h3 CONTACT US
+                dl
+                    dt
+                        icon(name="map-marker")
+                    dd
+                        p 185 Nanjing Xi Lu, Jing’an
+                        p Qu, Shanghai, China 20000
+                dl
+                    dt
+                        icon(name="envelope-o")
+                    dd
+                        p info@canvas.org
+                dl
+                    dt
+                        icon(name="phone")
+                    dd
+                        p (+86) 323 555 1111
+            li.follow
+                h3 FOLLOW US
+                img.qrcode(src="../assets/qrcode.jpg" width="150" height="150")
 </template>
 
 <script>
@@ -22,11 +75,11 @@ export default {
                 },
                 {
                     desc: 'Tell your story',
-                    url: require('../assets/road.jpeg')
+                    url: require('../assets/mic.jpg')
                 },
                 {
                     desc: 'Engage with your school',
-                    url: require('../assets/road.jpeg')
+                    url: require('../assets/university.jpg')
                 }
             ]
         }
@@ -34,7 +87,118 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less">
+.launch {
+    font-family: monospace;
+    > .rd-swipe {
+        width: 100%;
+    }
+    >.intro {
+        padding: 30px 0;
+        > ul {
+            display: flex;
+            justify-content: center;
+            list-style: none;
+            > li {
+                padding: 0 6%;
+                width: 12%;
+                > img {
+                    width: 100%;
+                }
+                > h3 {
+                    margin-top: 10px;
+                    text-align: center;
+                }
+                > h3, > p {
+                    font-weight: bolder;
+                    font-size: 20px;
+                }
+            }
+        }
+        > .login {
+            margin-top: 3%;
+            text-align: center;
+            > .wrap {
+                position: relative;
+                margin: auto;
+                padding: 30px 0;
+                width: 52%;
+                height: 400px;
+                background: url('../assets/login.jpeg') no-repeat center center;
+                background-size: cover;
+                > h3 {
+                    color: #fff;
+                    font-size: 300%;
+                }
+                > input {
+                    display: block;
+                    margin: 30px auto;
+                    width: 85%;
+                    height: 40px;
+                    text-align: center;
+                    font-size: 200%;
 
+                }
+
+                ::-webkit-input-placeholder { /* WebKit, Blink, Edge */
+                    color: #a568d0;
+                }
+                :-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+                   color: #a568d0;
+                }
+                ::-moz-placeholder { /* Mozilla Firefox 19+ */
+                   color: #a568d0;
+                }
+                :-ms-input-placeholder { /* Internet Explorer 10-11 */
+                   color: #a568d0;
+                }
+
+                > p {
+                    position: absolute;
+                    bottom: 40px;
+                    width: 100%;
+                    color: #fff;
+                    font-weight: bolder;
+                    font-size: 350%;
+                }
+            }
+        }
+    }
+
+    > .contact {
+        display: flex;
+        background: #edeff2;
+        padding: 5%;
+        list-style: none;
+        > li {
+            flex: 1;
+            > h3 {
+                font-size: 30px;
+            }
+            > p {
+                font-size: 18px;
+            }
+        }
+        > .contact {
+            > dl {
+                display: flex;
+                align-items: center;
+                border-bottom: 1px solid #9f9fa0;
+                > dt {
+                    width: 42px;
+                    height: 42px;
+                    padding: 0 10px;
+                }
+                > dd {
+                    flex: 1;
+                }
+            }
+        }
+        > .follow {
+            > img {
+                margin: 5px;
+            }
+        }
+    }
+}
 </style>
