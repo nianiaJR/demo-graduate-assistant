9125902d87e92ed852b379f4685433642159325e
梁家瑞 <ljracm@gmail.com>
2017-03-22 09:49:40 +0800

    ADD: init person

---
+++ src/components/People.vue
@@ -0,0 +1,93 @@
+<style lang="less">
+</style>
+<template lang="pug">
+    .people
+        .resume
+            img(src="../assets/girl.jpg" width="100" height="100")
+            .desc
+                h3 Liang Li
+                p Shanghai I China
+                p Shanghai High School I Grade 10
+                .contact
+                    p Canvas.com/s/bi-yue
+                    p
+                      icon(name="envolope")
+                      span Contact
+        ul
+            li
+                h3 MY STORY
+                p Hi! I am Ling from Shanghai. I love visual art and journalism! My dream is to empower Chinese youth to use art to express their individuality and their dream for society. Check out my profile!
+                ul.goods
+                    li
+                        img(src="../assets/skill.png")
+                        p SKILLS
+                        .desc
+                            span Writing
+                            span Event Curation
+                            span Communication
+                            span Teamwork
+                            span Photography
+                    li
+                        img(src="../assets/passion.png")
+                        p PASSIONS
+                        .desc
+                            span Art History
+                            span Travelling
+                    li
+                        img(src="../assets/language.png")
+                        p LANGUAGE(S)
+                        .desc
+                            span Mandarin
+                            span English
+                            span French
+            li
+                h3 MY ACTIVITIES
+                ul
+                    li
+                        .desc
+                            h4 President, Shanghai High School Newspaper
+                            p.dot Curated the print and online version of school’s monthly newspaper 
+                            p.dot Managed a team of 7 students to develop content 
+                            .imgs
+                                .img
+                                    img(src="../assets/football.jpg")
+                                    p New PE Teacher,New Sport for Girls
+                                .img
+                                    img(src="../assets/conference.jpg")
+                                    p Take Your Team Discussion Underwater
+
+                        .time
+                            H3 Jan 2016 - Present
+                            p
+                                img(src="../assets/gold.png")
+                                span Endorsements
+                    li
+                        .desc
+                            h4 Curation Manager, Ju Lu High School Annual Art Festival
+                            p.dot Partnered with Disney Shanghai to promote storytelling among students
+                            p.dot 500 students and 200 parents attended the event  
+                            .img
+                                img(src="../assets/disney.jpg")
+                            .img
+                                img(src="../assets/disney2.jpg")
+                .see-more
+                    span see more
+                    icon(name="angle-down")
+            li
+                h3 WHAT YOU SHOULD KNOW ABOUT ME
+                p.dot I am not good at math
+                p.dot Have a dream to publish a picture book about old Shanghai architectures and their stories
+                p.dot My parents want me to study economics in university but I am going to secretly change it to art history 
+                p.dot I have two fat cats whom I wish I could to university (just like how Hogwarts allow pets)
+                .see-more
+                    span see more
+                    icon(name="angle-down")
+            li
+                h3 ARTICLES I LOVE
+                .dec
+                    img(scr="../assets/life.png")
+                    h3 Social entrepreneurs take spotlight at TEDWomen
+                    p desc descdesdcda
+</template>
+<script>
+</script>
